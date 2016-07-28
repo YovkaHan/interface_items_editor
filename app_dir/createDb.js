00fdc35efac9b42e846d3657a618b739224fcd75
Ihor Chaikovskiy <yovkahan@gmail.com>
2016-07-28 13:55:30 +0300

    some edition to app_dir

--- app_dir/createDb.js
+++ app_dir/createDb.js
@@ -1,23 +1,44 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/interface-editor-app');
+var mongoose = require('./libs/mongoose');
+var async = require('async');
 
-var schema = mongoose.Schema({
-    name: String
-});
-schema.methods.meow = function(){
-    console.log(this.get('name'));
-};
-
-var Cat = mongoose.model('Cat', schema);
-
-var kitty = new Cat({
-    name: 'Zildjian'
+async.series([
+    open,
+    dropDatabase,
+    requireModels,
+    createUsers,
+], function(err) {
+    console.log(arguments);
+    mongoose.disconnect();
+    process.exit(err ? 255: 0);
 });
 
-kitty.save(function (err, kitty, affected) {
-  if (err) {
-    console.log(err);
-  } else {
-      kitty.meow();
-  }
-});
+function open(callback) {
+ mongoose.connection.on('open', callback);
+}
+
+function dropDatabase(callback) {
+    var db = mongoose.connection.db;
+    db.dropDatabase(callback);
+}
+
+function requireModels(callback) {
+    require('./models/user');
+
+    async.each(Object.keys(mongoose.models), function(modelName, callback) {
+        mongoose.models[modelName].ensureIndexes(callback);
+    }, callback);
+}
+
+function createUsers(callback) {
+
+    var users = [
+        {userbane: 'Вася', password: 'supervasya'},
+        {userbane: 'Игорь', password: '123'},
+        {userbane: 'admin', password: 'admin'}
+    ];
+
+    async.each(users, function(userData, callback) {
+        var user = new mongoose.models.User(userData);
+        user.save(callback);
+    }, callback);
+}
